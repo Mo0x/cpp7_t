e4c9e817892e6ceee99df08146144fd6401fac24
Mo0x <max.govindaraju@gmail.com>
2025-01-24 15:48:04 +0100

    main for ex02

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phuture <phuture@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:31:16 by phuture           #+#    #+#             */
-/*   Updated: 2025/01/24 14:21:49 by phuture          ###   ########.fr       */
+/*   Updated: 2025/01/24 15:47:48 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,20 @@ class Array
     }
 
     Array(Array const &src) :
-    m_size(src.size)
+    m_size(src.size())
     {
         this->m_arr = NULL;
         *this = src;
     }
 
-    Array &operator=(Array const *src)
+    Array &operator=(Array const &src)
     {
         if (this->m_arr != NULL)
             delete [] this->m_arr;
-        if (src.size != 0)
+        if (src.size() != 0)
         {
             this->m_size = src.size();
-            this->m_arr = new T[this->size];
+            this->m_arr = new T[this->m_size];
             for (size_t i = 0; i < this->m_size; i++)
             this->m_arr[i] = src.m_arr[i];
         }
@@ -60,7 +60,7 @@ class Array
 
     T &operator[](size_t index)
     {
-        if(index >= this->m_size || this->m_arr = NULL)
+        if(index >= this->m_size || this->m_arr == NULL)
         {
             throw Array::OutOfBound();
         }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phuture <phuture@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:05:09 by phuture           #+#    #+#             */
-/*   Updated: 2025/01/24 14:08:17 by phuture          ###   ########.fr       */
+/*   Updated: 2025/01/24 15:47:15 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
 #include "Array.hpp"
+
+#define MAX_VAL 999
 
 int main(void)
 {
@@ -19,6 +22,103 @@ int main(void)
     for (size_t i = 0; i < num.size(); i++)
         num[i] = i;
     for (size_t i = 0; i < num.size(); i++)
-        std::cout << num[i];
+	{
+        std::cout << num[i] << std::endl;
+	}
+	Array<std::string> strings(2);
+	strings[1] = "lol";
+	std::cout << strings[1] << std::endl;
     return (0);
+	{
+		Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp;
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+                std::cout << "tmp[" << i << "]:\t" << tmp[i] << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+        tmp = numbers;
+        Array<int> test(tmp);
+
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+        {
+            if (tmp[i] != test[i])
+            {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+            // std::cout << "tmp[" << i << "]:\t" << tmp[i] << std::endl; //uncomment these two lines to see the arrays have the same values
+            // std::cout << "test[" << i << "]:\t" << test[i] << std::endl;
+        }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+
+        std::cout << std::endl << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
+
+        test[1] = 123456789;
+
+        std::cout << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        // std::cout << "index was -2" << std::endl;
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        // std::cout << "tried to access past the last element of the array" << std::endl;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     std::cout << numbers[i] << std::endl;
+    // }
+    return 0;
+	}
 }
